48995c008453f98cb918f6852c8864dc1ecfcd34
Trishan Singh <trishan_singh@mentor.com>
2012-12-20 16:28:01 -0800

    Adding the CI php framework.

---
+++ application/views/default_start.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tsingh
+ * Date: 12/20/12
+ * Time: 06:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+?>
+And the Default start page data goes here.
+is there any wrapper data at this point?
+
+<h2> Good Question </h2>
+
+    <p>Go get this url</p>
+http://www.steepandcheap.com/steepcheap/sac/jsdata.js?rand=55380
+    <p>JSON data</p>
+<pre>{
+"pplos": {
+"history": [
+"5166",
+"5043",
+"4950",
+"4943",
+"4936",
+"4902"
+],
+"interval": 60
+},
+"absolute_times": 1,
+"refreshInterval": 20000,
+"prev_items": [
+1355955360,
+1356024316,
+1356037494,
+1356038398,
+1356039600
+],
+"currentItem": {
+"maxPurchase": 3,
+"totalRemaining": 11,
+"totalTime": 660,
+"productTitle": "Kelty Coromell CP Sleeping Bag: 25 Degree Synthetic",
+"variants": {
+"KEL0890_ONECOL_LONZIP": {
+"images": {
+"smallImage": "http://www.backcountry.com/images/items/tiny/KEL/KEL0890/ONECOL.jpg",
+"mediumImage": null,
+"bigImage": "http://www.backcountry.com/images/items/large/KEL/KEL0890/ONECOL.jpg"
+},
+"sku": "KEL0890-ONECOL-LONZIP",
+"max_purchase": 3,
+"totalInv": 9,
+"onHand": 7,
+"description": "One Color, Long/Right Zip",
+"display_sort": 2
+},
+"KEL0890_ONECOL_REGZIP": {
+"images": {
+"smallImage": "http://www.backcountry.com/images/items/tiny/KEL/KEL0890/ONECOL.jpg",
+"mediumImage": null,
+"bigImage": "http://www.backcountry.com/images/items/large/KEL/KEL0890/ONECOL.jpg"
+},
+"sku": "KEL0890-ONECOL-REGZIP",
+"max_purchase": 3,
+"totalInv": 8,
+"onHand": 4,
+"description": "One Color, Reg/Right Zip",
+"display_sort": 1
+}
+},
+"fullPrice": "129.95",
+"end_time": 1356040264,
+"percentOff": 64,
+"skuClass": "KEL0890",
+"price": "46.95",
+"odatId": 375728,
+"soldOutImage": "http://images.steepandcheap.com/images/steepcheap/sold_out_variant.gif"
+},
+"marquee": {
+"message": "<style type='text/css'>#marquee_text a, #marquee_text a:visited {color: #fff; text-decoration: underline;} #marquee_text a, #marquee_text a:hover, #marquee_text a:active {color: #fff; text-decoration: underline;}</style>Want an even bigger discount? Join our <a href='http://bit.ly/QwJkuR' target=_blank title='Software Engineer (ODAT team)'>software development team</a>."
+}
+
+}
+</pre>
+
+
